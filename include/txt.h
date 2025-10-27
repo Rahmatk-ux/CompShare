29218dadd7a42b575303d0dfd7d5ab45000745f9
Rahmatk-ux <rahmatk.9545@gmail.com>
2025-10-27 21:13:28 +0530

    Text header file modifies

---
+++ include/txt.h
@@ -0,0 +1,7 @@
+#ifndef TEXT_COMPRESSION_H
+#define TEXT_COMPRESSION_H
+
+void compressText(const char* inputFile, const char* outputFile);
+void decompressText(const char* inputFile, const char* outputFile);
+
+#endif
