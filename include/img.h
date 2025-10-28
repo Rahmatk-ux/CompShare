f2c6d2f3ccab3240ecae443484e69c3530db1962
Rahmatk-ux <rahmatk.9545@gmail.com>
2025-10-29 04:51:31 +0530

    Update image.h

---
+++ include/img.h
@@ -0,0 +1,7 @@
+#ifndef IMAGE_COMPRESSION_H
+#define IMAGE_COMPRESSION_H
+
+void compressImage(const char* inputFile, const char* outputFile);
+void decompressImage(const char* inputFile, const char* outputFile);
+
+#endif
